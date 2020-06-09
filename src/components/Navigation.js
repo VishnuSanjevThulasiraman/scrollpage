c41777366c4020cda74c4ed7e8376e96bf3c40c4
VishnuSanjevThulasiraman <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2020-06-09 16:03:21 +0530

    Create a Navigation Bar component and a components directory

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../App.css'
+
+function NavigationBar () {
+    return(
+        <div class="nav-bar">
+            <h1>
+                Unbunked
+            </h1>
+        </div>
+    );
+}
+
+export default NavigationBar;
