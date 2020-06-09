498c7e9336f0718fb51b24b135f21393affd196f
VishnuSanjevThulasiraman <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2020-06-09 14:54:38 +0530

    Created a Blog Bomponent but without any prominent features

---
+++ src/blog.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Blog() {
+    return(
+        <div>
+            <h1>
+                Blog
+            </h1>
+            <h2>
+                T SANJEV VISHNU
+            </h2>
+        </div>
+    );
+}
+
+export default Blog;
