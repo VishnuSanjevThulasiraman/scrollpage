31bba4186def667e7913923158227e9114291995
VishnuSanjevThulasiraman <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2020-06-09 21:43:29 +0530

    removed margins since now bootstrap is used

--- src/Dynamci.js
+++ src/Dynamci.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const Dynamci = styled.div 
 `
-margin-top: -22px;
+
 height: 4px;
 position : fixed;
 background: ${props => props.len ? "rgb(58,58,58)" : "white "};
@@ -12,7 +12,7 @@ width: ${props => props.len}%;
 
 const D = styled.div
 `
-margin-top: -22px;
+
 height: 4px;
 position : fixed;
 background: rgb(251,226,76);
