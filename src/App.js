6e85864fb7992e5234000cffa8e79800d541048f
VishnuSanjevThulasiraman <52427253+VishnuSanjevThulasiraman@users.noreply.github.com>
2020-06-09 16:04:21 +0530

    Imported and added the navigation component to the main app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Scoll from './Scroll';
 import Blog from './blog';
+import NavigationBar from './components/Navigation';
 
 export default class App extends Component {
 
@@ -9,6 +10,7 @@ export default class App extends Component {
     return (
       <div className="App">
         <Scoll />
+        <NavigationBar />
         <Blog />
         <Blog />
         <Blog />
